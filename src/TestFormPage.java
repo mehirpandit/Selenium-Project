639ad157d518d7bace48d5fbd363523146b1f807
mehirpandit <mp91117@uga.edu>
2021-07-07 18:07:12 -0400

    added junit assert

--- src/TestFormPage.java
+++ src/TestFormPage.java
@@ -1,7 +1,12 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestFormPage {
 
@@ -24,7 +29,15 @@ public class TestFormPage {
         driver.findElement(By.id("datepicker")).sendKeys("07/07/2021");
         driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
 
+        //Thread.sleep(5000);
+
         driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement alert = wait.until((ExpectedConditions.visibilityOfElementLocated(By.className("alert"))));
+        String alertText = alert.getText();
+        assertEquals("The form was successfully submitted!", alertText);
+        System.out.println(alertText);
 
         driver.close();
 
